060efe2e15a4108bea3b408798a4a96ab9450c34
Max <59546581+Maxisthemoose@users.noreply.github.com>
2023-01-09 22:59:49 -0800

    Nice job me

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -4,3 +4,4 @@ mongoose.connect("mongodb+srv://User:1nMqk8xdUrVbcnOQ@cluster0.gr3dp.mongodb.net
   if (err) console.log(err)
   else console.log("Logged into mongodb")
 });
+// WOOO Still have not removed this from the code hahaha
