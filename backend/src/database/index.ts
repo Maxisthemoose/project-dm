b4318bfcc006ac19f92507d6f0124a012d6f1e4b
Maxisthemoose <oriontothemax@gmail.com>
2023-01-09 23:03:30 -0800

    finally lmao

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://User:1nMqk8xdUrVbcnOQ@cluster0.gr3dp.mongodb.net/?retryWrites=true&w=majority", {}, (err) => {
+import { mongo_uri } from "../config/config.json";
+mongoose.connect(mongo_uri, {}, (err) => {
   if (err) console.log(err)
   else console.log("Logged into mongodb")
 });
-// WOOO Still have not removed this from the code hahaha
